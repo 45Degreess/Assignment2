e309c97f84d29f4036e7123078d456720d95502d
Chidiebere umah <chidie.umah@gmail.com>
2024-03-21 00:35:23 +0200

    Added Database connection class

---
+++ src/DB.java
@@ -0,0 +1,40 @@
+package Assignment_2.src;
+
+import java.sql.*;
+public class DB
+{
+    //Relative path in the folder of project
+    private static final String url = "jdbc:ucanaccess://Assignment_2/lib/Assignment2.accdb";
+    
+    private Connection conn;
+    
+    private PreparedStatement statement;
+    private ResultSet resultSet;
+    
+    public DB() throws ClassNotFoundException
+    {
+        try
+        {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            conn = DriverManager.getConnection(url);
+            
+        } catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void update(String update) throws SQLException
+    {
+        statement = conn.prepareStatement(update);
+        statement.executeUpdate();
+        statement.close();
+    }
+    
+    public ResultSet query(String stmt) throws SQLException
+    {
+        statement = conn.prepareStatement(stmt);
+        resultSet = statement.executeQuery();
+        return resultSet;
+    }
+}
